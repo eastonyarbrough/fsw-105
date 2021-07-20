5757d0188100d8452ae4a85192f9eea9cf92a51c
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2021-07-20 17:22:46 -0500

    Added healing mechanic and adjusted random generation of loot

--- buildingARolePlayingGameInNode/buildingARolePlayingGameInNode.js
+++ buildingARolePlayingGameInNode/buildingARolePlayingGameInNode.js
@@ -8,7 +8,7 @@ const start = readline.keyIn(`${playerName} please press 1 to start.`, {limit: '
 let pressStart = false;
 if (start == 1)
 {
-    console.log("You're standing on a road in between two towns. Bandits and monsters plague this area! Be careful out here!");
+    console.log("You're standing on a road between two towns. Bandits and monsters plague this area! Be careful out here!");
     pressStart = true;
 }
 
@@ -17,20 +17,22 @@ const enemies = ["bandit", "goblin", "kobold", "troll", "giant"];
 const treasure = ["Sword", "Magic Talisman", "Coin Purse", "Trinket"];
 let inventory = [];
 let userHealth = 100;
-let pickUp = treasure[Math.floor(Math.random() * treasure.length)];
+let hasHealed = false;
 
 function game()
 {
     while (pressStart == true && userHealth > 0)
     {
         //Randomized Variables.
+        let pickUp = treasure[Math.floor(Math.random() * treasure.length)];
+        let enemyHealth = Math.floor(Math.random() * 200);
         const attackPower = Math.floor(Math.random() * (30 + 15 - 3) + 15);
         const enemy = enemies[Math.floor(Math.random() * enemies.length)];
-        let enemyHealth = Math.floor(Math.random() * 150);
         const enemyPower = Math.floor(Math.random() * (20 + 20 - 2) + 12);
+        const heal = Math.floor(Math.random() * 70);
 
         //Action Options.
-        const perform = readline.keyIn("What would you like to do? \nPress 'W' to walk. \nPress 'P' to print stats. \nPress 'X' to exit the game.", {limit: '$<w, p, x>'});
+        const perform = readline.keyIn("What would you like to do? \nPress 'W' to walk. \nPress 'H' to heal. \nPress 'P' to print stats. \nPress 'X' to exit the game.", {limit: '$<w, h, p, x>'});
 
         //Game Mechanics.
         if (perform == 'x')
@@ -39,7 +41,28 @@ function game()
         }
         else if (perform == 'p')
         {
-            console.log(`Name: ${playerName} \nHealth: ${userHealth} \nItems: ${inventory}`);
+            console.log(`Name: ${playerName} \nHealth: ${userHealth} \nItems:${inventory}`);
+        }
+        else if (perform == 'h')
+        {
+            if (userHealth < 100 && hasHealed == false)
+            {
+                userHealth += heal;
+                console.log(`You healed for ${heal} hit points!`);
+                hasHealed = true;
+                if (userHealth > 100)
+                {
+                    userHealth = 100;
+                }
+            }
+            else if (userHealth < 100 && hasHealed == true)
+            {
+                console.log(`You have already healed! - Health: ${userHealth}`)
+            }
+            else if (userHealth >= 100)
+            {
+                console.log(`Your health is full! - Health: ${userHealth}`)
+            }
         }
         else if (perform == 'w')
         {
@@ -54,12 +77,19 @@ function game()
                 while (userHealth > 0 && enemyHealth > 0)
                 {
                     const actions = readline.keyIn("What would you like to do? \nPress 'R' to run away. \nPress 'A' to attack.", {limit: '<r, a>'});
+                    hasHealed = false;
                     if (actions == 'r')
                     {
                         const run = Math.random();
                         if (run <= 0.5)
                         {
-                            console.log(`You couldn't get away! ${enemy} attacks you for ${enemyPower} damage!`);
+                            console.log(`You couldn't get away! The ${enemy} attacks you for ${enemyPower} damage!`);
+                            userHealth -= enemyPower;
+                            if (userHealth <= 0)
+                            {
+                                console.log(`The ${enemy} has slain you! ${playerName} is dead!`);
+                                break;
+                            }
                         }
                         else if (run >= 0.51)
                         {
@@ -80,7 +110,7 @@ function game()
                             if (dropLoot <= 0.25)
                             {
                                 console.log(`You found a ${pickUp} on the dead ${enemy}!`);
-                                inventory.push(pickUp);
+                                inventory.push(" " + pickUp);
                             }
                         }
                         if (userHealth <= 0)
